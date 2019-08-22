2222f217636715214dcfa61254b1c0a1c7dea5c5
JACJava <46228469+JACJava@users.noreply.github.com>
2019-08-22 18:06:39 -0500

    API Gateway

---
+++ scripts/module_06/deploy-api-gateway.js
@@ -0,0 +1,25 @@
+// Imports
+const AWS = require('aws-sdk')
+
+AWS.config.update({ region: 'us-east-2' })
+
+// Declare local variables
+const apiG = new AWS.APIGateway()
+const apiId = '5wb0gmxq9i'
+
+createDeployment(apiId, 'prod')
+.then(data => console.log(data))
+
+function createDeployment (apiId, stageName) {
+  const params = {
+    restApiId: apiId,
+    stageName: stageName
+  }
+
+  return new Promise((resolve, reject) => {
+    apiG.createDeployment(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
