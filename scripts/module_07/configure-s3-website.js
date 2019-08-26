d96eaaebce2566b1cdaacf43818a7e38a5c05bce
JACJava <46228469+JACJava@users.noreply.github.com>
2019-08-26 13:21:49 -0500

    S3 Static Website

--- scripts/module_07/configure-s3-website.js
+++ scripts/module_07/configure-s3-website.js
@@ -1,18 +1,30 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-2' })
 
 // Declare local variables
 const s3 = new AWS.S3()
 
-configureS3Site('/* TODO: Add your S3 bucket name */')
+configureS3Site('hamster-bucket-julieaws')
 .then(data => console.log(data))
 
 function configureS3Site (bucketName) {
-  // TODO: Create params const object
+  // Create params const object
+  const params = {
+    Bucket:  bucketName,
+    WebsiteConfiguration:  {
+      IndexDocument:  {
+        Suffix: 'index.html'
+      }
+    }
+  }
 
   return new Promise((resolve, reject) => {
     // Call putBucketWebsite
+    s3.putBucketWebsite(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
