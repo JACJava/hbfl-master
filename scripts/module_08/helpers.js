5851a8d6f3953272bf6470d0ce310f03b7d541a2
JACJava <46228469+JACJava@users.noreply.github.com>
2019-09-12 16:16:45 -0500

    jeez

--- scripts/module_08/helpers.js
+++ scripts/module_08/helpers.js
@@ -38,13 +38,15 @@ function createLambdaKinesisRole2 () {
   const params = {
     RoleName: roleName
   }
+
   return new Promise((resolve, reject) => {
-      iam.getRole(params, (err) => {
-          if (err) reject(err)
-          else resolve(data.Role.Arn)
-        })
+    iam.getRole(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data.Role.Arn)
     })
+  })
 }
+
 
 function zipLambdaFile () {
   const archive = archiver('zip', {
