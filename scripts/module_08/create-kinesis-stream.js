28d05f08ba1484a5760618f7f11f28af5c5a3c3d
JACJava <46228469+JACJava@users.noreply.github.com>
2019-09-12 14:22:02 -0500

    added kinesis push (but not consume)

--- scripts/module_08/create-kinesis-stream.js
+++ scripts/module_08/create-kinesis-stream.js
@@ -1,19 +1,27 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-2' })
 
 // Declare local variables
 // TODO: Create kinesis object
+const kinesis = new AWS.Kinesis()
 const streamName = 'hamster-race-results'
 
 createKinesisStream(streamName)
 .then(data => console.log(data))
 
 function createKinesisStream (streamName) {
-  // TODO: Create params const
-
+  // Create params const
+  const params = {
+    ShardCount: 1,
+    StreamName:  streamName
+  }
   return new Promise((resolve, reject) => {
-    // TODO: Create kinesis stream
+    //Create kinesis stream
+    kinesis.createStream(params, (err, data) => {
+      if (err) reject(err)
+      else resolve()
+    })
   })
 }
