e4df1d1f1318c3eb44fa1f3e5d0c934c2aacc4dc
JACJava <46228469+JACJava@users.noreply.github.com>
2019-09-13 14:17:43 -0500

    communicating with aws module

--- scripts/module_09/create-sns-subscription.js
+++ scripts/module_09/create-sns-subscription.js
@@ -1,21 +1,29 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-2' })
 
 // Declare local variables
 const sns = new AWS.SNS()
-const type = 'sms'
-const endpoint = '/* TODO: Add your mobile number with country code */'
-const topicArn = '/* TODO: Add your sns topic arn */'
+const type = 'email'
+const endpoint = ''
+const topicArn = 'arn:aws:sns:us-east-2:688220141166:hamster-topic'
 
 createSubscription(type, topicArn, endpoint)
 .then(data => console.log(data))
 
 function createSubscription (type, topicArn, endpoint) {
-  // TODO: Create params const
-
+  // params const
+  const params = {
+    Protocol:  type,
+    TopicArn:  topicArn,
+    Endpoint:  endpoint
+  }
   return new Promise((resolve, reject) => {
-    // TODO: Subscribe
+    // Subscribe
+    sns.subscribe(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
--- scripts/module_09/create-sns-topic.js
+++ scripts/module_09/create-sns-topic.js
@@ -1,19 +1,27 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-2' })
 
 // Declare local variables
-// TODO: Create sns object
+// Create sns object
+const sns = new AWS.SNS()
 const topicName = 'hamster-topic'
 
 createTopic(topicName)
 .then(data => console.log(data))
 
 function createTopic (topicName) {
-  // TODO: Create params const
+  // Create params const
+  const params = {
+    Name:  topicName
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create topic
+    // Create topic
+    sns.createTopic(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
